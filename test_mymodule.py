c9d24f9e5eb0c4ab14d1ede6280d2221e388e509
Doheera-kosi <kupourevans99@gmail.com>
2023-05-01 00:25:53 +0000

    Update test_mymodule.py

--- test_mymodule.py
+++ test_mymodule.py
@@ -13,5 +13,25 @@ class TestDouble(unittest.TestCase):
         self.assertEqual(double(2), 4) # test when 2 is given as input the output is 4.
         self.assertEqual(double(-3.1), -6.2) # test when -3.1 is given as input the output is -6.2.
         self.assertEqual(double(0), 0) # test when 0 is given as input the output is 0.
+
+class TestAdd(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add(2, 4), 6)
+
+    def test1(self):
+        self.assertEqual(add(0, 0), 0)
+
+    def test1(self):
+        self.assertEqual(add(2.3, 3.6), 5.9)
+
+    def test1(self):
+        self.assertEqual(add('hello', 'world'), 'helloworld')
+
+    def test1(self):
+        self.assertAlmostEqual(add(2.3000, 4.300), 6.6)
+
+    def test1(self):
+        self.assertNotEqual(add(-2, -2), 0)
+
         
 unittest.main()
